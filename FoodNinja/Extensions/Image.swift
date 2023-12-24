e535172e4fe3c260f7f6f7c84a2c314c909b173b
Imran Malik <amymalik27@gmail.com>
2023-12-24 13:19:53 +0530

    Part 1- Splash View

--- FoodNinja/Extensions/Image.swift
+++ FoodNinja/Extensions/Image.swift
@@ -5,4 +5,14 @@
 //  Created by Imran on 24/12/23.
 //
 
-import Foundation
+import SwiftUI
+
+enum ImageName: String {
+    case splash, splashLogo
+}
+
+extension Image {
+    init(name: ImageName) {
+        self.init(name.rawValue)
+    }
+}
