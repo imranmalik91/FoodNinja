b0a646414332479847fe901487d9c227f6b99c05
Imran Malik <amymalik27@gmail.com>
2023-12-24 12:57:21 +0530

    Initial Commit

---
+++ FoodNinja/FoodNinjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodNinjaApp.swift
+//  FoodNinja
+//
+//  Created by Imran on 24/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodNinjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
